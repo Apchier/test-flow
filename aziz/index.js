95fc564e10ce700522ea3dbdd8370e00538ec38d
avnexcode <axnvee18@gmail.com>
2024-11-30 21:02:51 +0700

    feat(aziz): create fetch api

--- aziz/index.js
+++ aziz/index.js
@@ -1,2 +1,9 @@
 console.log('test');
 console.log('ini coba update');
+
+const fetchApi = (url, callback) =>
+	fetch(url)
+		.then(response => response.json())
+		.then(data => callback(data));
+
+fetchApi('https://fakestoreapi.com/products', data => console.log(data));
